a1f380afbacccc62ac854cceca0c843393e8d9ca
Luvz2Fly <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-06 00:57:10 -0400

    Add a new word Version 2

--- vowel.py
+++ vowel.py
@@ -1,7 +1,6 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
-word = "Milliways"
+word = "New Word Version 2"
 for letter in word:
     if letter in word:
         if letter in vowels:
             print(letter)
-            
