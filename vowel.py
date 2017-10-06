ba565e127b8008ca1a1222018d52feff124ae2c4
Luvz2Fly <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-06 01:02:12 -0400

    Added append feature

--- vowel.py
+++ vowel.py
@@ -1,6 +1,9 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = "New Word Version 2"
+found = []
 for letter in word:
-    if letter in word:
-        if letter in vowels:
-            print(letter)
+    if letter in vowels:
+        if letter not in found:
+                found.append(letter)
+for vowel in found:
+    print(vowel)
