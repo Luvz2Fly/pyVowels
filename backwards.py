3e5a8f6eb7f83148c5c2168df47f56e6e87ed586
Luvz2Fly <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-09 00:15:51 -0400

    Added Runtime Loop to keep program going loop() run()

--- backwards.py
+++ backwards.py
@@ -1,11 +1,18 @@
 # Take an input and make it print out backwards
+def loop():
+    data = input("Type something here and I will print it backwards: ")
+    backwards = list(data)
+    backwardsOutput = ''.join(backwards[::-1])
+    print(backwardsOutput)
 
-data = input("Type something here and I will print it backwards: ")
-backwards = list(data)
-backwardsOutput = ''.join(backwards[::-1])
-print(backwardsOutput)
+    # Slim it down...
+    data = input("Type something here and I will print it backwards: ")
+    print(''.join(list(data)[::-1]))
 
-# Slim it down...
-data = input("Type something here and I will print it backwards: ")
-print(''.join(list(data)[::-1]))
+
+def run():
+    while True:
+        loop()
+
+run()
 
