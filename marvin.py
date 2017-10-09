8592fe6c8407e2b074effcc706836de3ca375aff
Luvz2Fly <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-09 00:28:31 -0400

    Initial Marvin c==

---
+++ marvin.py
@@ -0,0 +1,15 @@
+# Marvin is an example of a list call using parameters from start/stop/step
+
+paranoid_android = "Marvin, The Paranoid Android"
+letters = list(paranoid_android)
+
+for char in letters[:6]:
+    print('\t', char)
+print()
+
+for char in letters[-7:]:
+    print('\t' * 2, char)
+print()
+
+for char in letters[12:20]:
+    print('\t' * 3, char)
