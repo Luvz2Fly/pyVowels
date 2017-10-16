2110a5eec110e2cf748f7ecf75398d7f40d31b4d
Luvz2Fly <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-16 19:04:13 -0400

    Initial Marvin c==

--- backwards.py
+++ backwards.py
@@ -1,18 +1,25 @@
 # Take an input and make it print out backwards
+
 def loop():
-    data = input("Type something here and I will print it backwards: ")
-    backwards = list(data)
-    backwardsOutput = ''.join(backwards[::-1])
-    print(backwardsOutput)
+    # data = input("Type something here and I will print it backwards: ")
+    # backwards = list(data)
+    # backwardsOutput = ''.join(backwards[::-1])
+    # print(backwardsOutput)
 
     # Slim it down...
     data = input("Type something here and I will print it backwards: ")
     print(''.join(list(data)[::-1]))
+    ask()
 
-
-def run():
-    while True:
+def ask():
+    again = input("Do you want to play again? (y/n)")
+    if again.lower() == "y":
         loop()
+    #while True:
+    else:
+        print("Thanks for playing")
 
-run()
+loop()
+
+
 
