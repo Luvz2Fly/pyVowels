1c68a16540ba88015db0ec0c60ded81f2fdaaf91
Luvz2Fly <5325923+Luvz2Fly@users.noreply.github.com>
2017-10-16 19:05:07 -0400

    Dictionary Stuff

---
+++ data.py
@@ -0,0 +1,22 @@
+# person3 =   {'Name': 'Ford Prefect',
+#            'Gender': 'Male',
+#             'Occupation':'Researcher',
+#             'Home Planet':'Not Earth'
+#              }
+#
+#
+# print(person3['Gender'])
+# person3['Age'] = 33
+# print(person3['Age'])
+
+found = {}
+found['a'] = 0
+found['e'] = 0
+found['i'] = 0
+found['o'] = 0
+found['u'] = 0
+print(found)
+print(found)
+for i in range(30):
+    found['e'] +=1
+    print(found)
